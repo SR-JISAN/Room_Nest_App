5d7f0912cecd68dbba62a27b299df7dfc2491ba5
SR-JISAN <dev.md.jisan@gmail.com>
2026-09-13 02:24:05 +0600

    catchAsync Add

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import httpStatus from "http-status";
 import { globalErrorHandler } from "./app/middleware/global.error";
 import cookieParser from "cookie-parser";
 import { notFound } from "./app/middleware/not.found";
+import { AuthRoute } from "./app/model/auth/auth.route";
 
 const app: Application = express();
 
@@ -21,6 +22,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+//all api routes 
+
+app.use("/api/auth", AuthRoute)
+
+
+
+
+
+
+
+//main routes
 app.get("/", async (req: Request, res: Response) => {
   res.status(httpStatus.OK).json({
     success: true,
---
+++ src/app/model/auth/auth.service.ts
@@ -0,0 +1,9 @@
+
+const register = async ()=>{
+
+};
+
+
+export const AuthService = {
+    register
+}
