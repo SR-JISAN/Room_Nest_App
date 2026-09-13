830a3114e1248ee79b30be5883ad1c90e6c4c998
SR-JISAN <dev.md.jisan@gmail.com>
2026-09-14 01:21:34 +0600

    email verify otp set to redis

--- src/app/model/auth/auth.service.ts
+++ src/app/model/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { prisma } from "../../lib/prisma";
 import AppError from "../../utils/appError";
 import type { IUser } from "./auth.interface";
 import httpStatus from "http-status"
+import crypto from "crypto"
+import { redisClient } from "../../lib/redis";
 
 const register = async (payload:IUser)=>{
      const {email,name,password,imageURL,profile} = payload
@@ -14,6 +16,18 @@ const register = async (payload:IUser)=>{
     if(isExistUser){
         throw new AppError(httpStatus.CONFLICT,"this email already exist. Try with another email")
     }
+
+    const otp = crypto.randomInt(100000,1000000).toString();
+
+    const key = `verify-register-email: ${email}`
+
+    await redisClient.set(key,otp,{
+        expiration:{
+            type:"EX",
+            value: 2*60
+        }
+    });
+    
 
     const result = await prisma.users.create({
       data: {
