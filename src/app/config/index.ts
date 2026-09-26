f914edcb78f3c0a7f92bcd21959020e6013b60e1
SR-JISAN <dev.md.jisan@gmail.com>
2026-09-26 16:19:01 +0600

    deploy done

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -2,9 +2,10 @@ import dotenv from "dotenv"
 import path from "path"
 
 
+
 dotenv.config({
     path: path.join(process.cwd(),".env")
-})
+});
 
 export default {
   node_env: process.env.NODE_ENV,
